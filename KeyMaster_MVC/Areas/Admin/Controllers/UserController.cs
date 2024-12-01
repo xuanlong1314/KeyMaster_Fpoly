db5ebb8a1bc33643acb4966af1a4d2bc714b24c4
Nguyen Xuan Long <xuanlong2k414@gmail.com>
2024-12-01 20:52:18 +0700

    Update_User

--- KeyMaster_MVC/Areas/Admin/Controllers/UserController.cs
+++ KeyMaster_MVC/Areas/Admin/Controllers/UserController.cs
@@ -1,7 +1,11 @@
-﻿using KeyMaster_MVC.Repository;
+﻿using KeyMaster_MVC.Models;
+using KeyMaster_MVC.Repository;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Drawing.Drawing2D;
 
 namespace KeyMaster_MVC.Areas.Admin.Controllers
 {
@@ -9,15 +13,88 @@ namespace KeyMaster_MVC.Areas.Admin.Controllers
     [Authorize]
     public class UserController : Controller
     {
-        private readonly DataContext _dataContext;
-        public UserController(DataContext context)
+        private readonly UserManager<AppUserModel> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserController(UserManager<AppUserModel> userManager, RoleManager<IdentityRole> roleManager)
         {
-            _dataContext = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
+        [HttpGet]
         [Route("Index")]
         public async Task<IActionResult> Index()
         {
-            return View(await _dataContext.Users.OrderByDescending(p => p.Id).ToListAsync());
+            return View(await _userManager.Users.OrderByDescending(p => p.Id).ToListAsync());
+        }
+        [HttpGet]
+        [Route("Create")]
+        public async Task<IActionResult> Create()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+            return View(new AppUserModel());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("Create")]
+        public async Task<IActionResult> Create(AppUserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var createUserResult = await _userManager.CreateAsync(user, user.PasswordHash);
+                if (createUserResult.Succeeded)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    foreach (var error in createUserResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View(user);
+                }
+            }
+            else
+            {
+                TempData["error"] = "Thêm user không thành công";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            var roles = await _roleManager.Roles.ToListAsync();
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+            return View(user);
+        }
+
+        [HttpGet]
+        [Route("Delete")]
+        public async Task<IActionResult> Delete(string Id)
+        {
+            if (string.IsNullOrEmpty(Id))
+            {
+                return NotFound();
+            }
+            var user = await _userManager.FindByIdAsync(Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var deleteResult = await _userManager.DeleteAsync(user);
+            if (!deleteResult.Succeeded)
+            {
+               return View("Error");
+            }
+            TempData["success"] = "User đã được xóa thành công";
+            return RedirectToAction("Index");
         }
     }
 }
--- KeyMaster_MVC/Models/AppUserModel.cs
+++ KeyMaster_MVC/Models/AppUserModel.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KeyMaster_MVC.Models
 {
     public class AppUserModel : IdentityUser
     {
         public string Occupation { get; set; }
+
+        // ForeignKey relationship to IdentityRole
+        [ForeignKey("RoleId")]
+        public string RoleId { get; set; }
+
+        // Navigation property to IdentityRole (optional, useful for accessing Role information directly)
+        public virtual IdentityRole Role { get; set; }
     }
 }
